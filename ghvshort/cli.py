27a8a7ca36d5f75113e1f523581ffb94d274a20b
Christian Schneider <christian.schneider@iserv.eu>
2025-12-30 21:25:11 +0100

    redacted some informations for privacy reasons

--- ghvshort/cli.py
+++ ghvshort/cli.py
@@ -39,10 +39,10 @@ def _maybe_export_after_change(settings: Settings, repo: Repo) -> None:
                     "short_url": f"{settings.base_url}/{link.slug}",
                     "url": link.url,
                     "code": link.code,
-                    "hits": link.hits,
+                    "hits": 0,
                     "not_before_at": link.not_before_at,
                     "expires_at": link.expires_at,
-                    "last_access_at": link.last_access_at,
+                    "last_access_at": None,
                 }
                 for link in links
             ],
